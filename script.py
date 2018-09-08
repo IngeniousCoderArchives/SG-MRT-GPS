13c08e6cb501a15d2cf8cad65bee3b5e2836083c
IngeniousCoder <eltontay11@gmail.com>
2018-09-08 13:13:43 +0800

    Add files via upload

---
+++ script.py
@@ -0,0 +1,29 @@
+"""
+LINE NO              LINE
+   1                 EAST WEST LINE
+   2                 NORTH SOUTH LIKE
+   3                 CIRCLE LINE
+   4                 NORTH EAST LINE
+   5                 DOWNTOWN LINE
+   6                 CIRCLE LINE BRANCH
+   7                 AIRPORT BRANCH
+   8                 BUKIT PANJANG LRT
+   9                 SENKANG LRT WEST
+   10                SENKANG LRT EAST
+   11                PUNGGOL LRT WEST
+   12                PUNGGOL LRT EAST
+"""
+
+data = {
+         "1" : {
+                 "1":["Pasir Ris",[]]
+                 "2":["Tampines",[5]]
+                 "3":["Simei",[]]
+                 "4":["Tanah Merah",[7]]
+                 "5":["Bedok",[]]
+                 "6":["Kembangan",[]]
+                 "7":["Eunos",[]]
+                 "8":["Paya Lebar"
+                }
+    }
+        
